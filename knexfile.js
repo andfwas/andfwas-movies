a1f0a39d652cfe2a45f5f8a5fca01adb508ac7a6
andfwas <andfwas@gmail.com>
2017-11-15 11:48:40 -0700

    installed dotenv and setup environment variables

--- knexfile.js
+++ knexfile.js
@@ -1,9 +1,13 @@
-// Update with your config settings.
+const env = require('dotenv')
 
 module.exports = {
 
   development: {
     client: 'pg',
     connection: 'postgres://localhost/people_movies'
+  },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL
   }
 };
